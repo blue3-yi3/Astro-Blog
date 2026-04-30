79e1745ec5ac6c7778d0226d7a7fec19c3f585a3
blue3-yi3 <yishanliang2002@gmail.com>
2026-04-30 19:26:10 +0800

    fix

--- src/content.config.ts
+++ src/content.config.ts
@@ -20,6 +20,8 @@ const projects = defineCollection({
     description: z.string(),
     date: z.coerce.date(),
     draft: z.boolean().optional(),
+    toc: z.boolean().optional(),
+    tags: z.array(z.string()).optional(),
     demoURL: z.string().optional(),
     repoURL: z.string().optional(),
     feishuURL: z.string().optional(),
